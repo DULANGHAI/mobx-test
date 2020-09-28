d96fecb63af8b77667e6e58939eb208c137b35ef
DULANGHAI <604857875@qq.com>
2020-09-28 18:10:37 +0800

    decorators

--- src/pages/Rank/index.js
+++ src/pages/Rank/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { observable } from 'mobx';
+import {observer} from 'mobx-react'
+
+const appState = observable({
+  timer: 0,
+})
+
+appState.resetTimer = function () {
+  appState.timer = 0;
+};
+
+setInterval(function () {
+  appState.timer += 1;
+}, 1000);
+
+@observer
+class TimerView extends React.Component {
+  render() {
+    return (
+      <button onClick={this.onReset.bind(this)}>
+        Seconds passed: {this.props.appState.timer}
+      </button>
+    );
+  }
+
+  onReset() {
+    this.props.appState.resetTimer();
+  }
+};
 
 function Rank(props) {
   return  (
-    <div>Rank</div>
+    <div>
+      Rank
+      <TimerView appState={appState}></TimerView>
+    </div>
   )
 }
 
